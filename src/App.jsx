ca935ff8eecd5f0cc8a2d9d7e0698550d12458b2
vishalmet <vishalmet003@gmail.com>
2024-02-05 22:21:57 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import '../src/App.css';
+import React from 'react';
+import ParticlesBg from './components/ParticlesBg';
+function App (){
+  return(
+    
+    <ParticlesBg />
+
+    
+    )
+}
+
+export default App;
