3eebb094f24d96529b23dc37e35d15b7b398510a
vishalmet <vishalmet003@gmail.com>
2024-02-05 22:53:22 +0530

    tsparticles setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import '../src/App.css';
 import React from 'react';
-import ParticlesBg from './components/ParticlesBg';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+
 function App (){
   return(
-    
-    <ParticlesBg />
+    <div >
 
-    
+    <Router>
+      <Routes>
+        <Route path="/" Component={Home} />
+        {/* <Route path="/about" Component={About}/>
+        <Route path="/works" Component={Works}/>
+        <Route path="/experience" Component={Experience}/>
+        <Route path="/securedapp" Component={SecureDApp}/> */}
+
+      </Routes>
+    </Router>
+
+    </div>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import ParticlesBg from '../components/ParticlesBg'
 
 const Home = () => {
   return (
     
-    <div>Home</div>
+    <div className="h-screen bg-cover bg-center">
+        <ParticlesBg/>
+        <div className="">
+            <p className=' text-red-500 '>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+            <p>Home</p>
+        </div>
+        
+    </div>
   )
 }
 
-export default Home
+export default Home;
