3151569eaa1173702e700c5abee36a4217271173
vishalmet <vishalmet003@gmail.com>
2024-03-05 20:21:12 +0530

    about-res

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -38,15 +38,19 @@ const About = () => {
 
         <div className=" pt-12 pb-12 flex-shrink-0" >
           <p className=' flex justify-center  pt-6 font-semibold poppins-font text-white text-2xl  md:text-4xl ' data-aos = "fade-up">ABOUT</p>
-          <div className=" md:flex justify-center items-center p-3 md:p-16 mx-3 md:mx-20 md:mt-12 rounded-lg flex-shrink-0 md:space-x-32 hover:scale-105 hover:border-2 hover:border-cyan-300 " data-aos = "flip-right" style={textBG}>
-            <div className=' space-y-4 md:text-2xl text-white poppins-font' data-aos = "fade-up"> 
+          <div className=" justify-center items-center p-3 mx-10 md:p-16 lg:mx-20  md:mt-12 rounded-lg flex-shrink-0  hover:scale-105 hover:border-2 hover:border-cyan-300 " data-aos = "flip-right" style={textBG}>
+            <div className="flex justify-center">
+              <img className=' h-auto rounded-lg  pt-2 md:pt-0 lg:h-96' data-aos = "fade-up" src={Me} alt="Vishal" />
+            </div>
+
+            <div className=' space-y-4 md:text-2xl text-white pt-4 poppins-font' data-aos = "fade-up"> 
                 <p>🚀 A dynamic Frontend Web Developer and an aspiring UI/UX Designer.</p> 
                 <p>🚀 I am fervently dedicated to continuous learning and embracing emerging technologies. </p>
                 <p>🚀 My passion extends to exploring the intricate world of blockchain technology, further enriching my skill set. </p>
                 <p>🚀  Committed to creating seamless, user-centric digital experiences. </p>
                 <p>🚀 I leverage my expertise to drive innovation in the ever-evolving landscape of web development and design.</p>
             </div>
-            <img className=' h-auto rounded-lg pt-2 md:pt-0 md:h-52' data-aos = "fade-up" src={Me} alt="Vishal" />
+            
           </div>
         </div>
         {/* <div className=" pt-12 pb-12">  
