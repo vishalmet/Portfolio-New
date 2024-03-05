24df082bcc96f3df9eb106c473fddba1abde3d41
vishalmet <vishalmet003@gmail.com>
2024-03-05 20:20:56 +0530

    nav-res

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,13 +29,13 @@ const Navbar = () => {
             <a href="/" className="hover:cursor-pointer">
                 <img className="h-12" src={Logo} alt="Logo" />
             </a>
-            <div className="md:hidden">
+            <div className="lg:hidden">
                 <button onClick={toggleMenu} className="block text-xl font-semibold focus:outline-none mr-4">
                     ☰ 
                 </button>
             </div>
-            <div className={`md:hidden fixed inset-0 bg-gray-900 bg-opacity-50 z-50 ${isOpen ? 'block' : 'hidden'}`} onClick={closeMenu}></div>
-            <div className={`md:hidden absolute inset-y-0 left-0 transform transition duration-300 ease-in-out bg-white w-64 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+            <div className={`lg:hidden fixed inset-0 bg-gray-900 bg-opacity-50 z-50 ${isOpen ? 'block' : 'hidden'}`} onClick={closeMenu}></div>
+            <div className={`lg:hidden absolute inset-y-0 left-0 transform transition duration-300 ease-in-out bg-white w-64 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
              <button onClick={handleCloseClick} className="absolute top-0 right-0 m-4 text-2xl focus:outline-none">×</button>
                 <ul className="p-4 space-y-2 poppins-font font-medium">
                     <li><Link to="/" className='py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black' >Home</Link></li>
@@ -47,7 +47,7 @@ const Navbar = () => {
                 </ul>
             </div>
             {/* Desktop Navbar */}
-            <div className="hidden md:flex md:items-center md:justify-center md:space-x-4  space-x-2 poppins-font font-medium mr-4 text-lg">
+            <div className="hidden lg:flex lg:items-center lg:justify-center lg:space-x-4  space-x-2 poppins-font font-medium mr-4 text-lg">
                 <div className="">
                     <Link to="/" className="py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black"> Home </Link>
                     {/* <Link to="#about-section" className="py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black"> About Me </Link> */}
