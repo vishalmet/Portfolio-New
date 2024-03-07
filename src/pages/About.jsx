e5b672c9b94b1d127bef86ea7f94f3d6f0b852f8
vishalmet <vishalmet003@gmail.com>
2024-03-07 20:18:47 +0530

    about-update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
           <p className=' flex justify-center  pt-6 font-semibold poppins-font text-white text-2xl  md:text-4xl ' data-aos = "fade-up">ABOUT</p>
           <div className=" justify-center items-center p-3 mx-6 md:mx-10 md:p-16 lg:mx-20  md:mt-12 rounded-lg flex-shrink-0  hover:scale-105 hover:border-2 hover:border-cyan-300 " data-aos = "flip-right" style={textBG}>
             <div className="flex justify-center">
-              <img className=' h-auto rounded-lg  pt-2 md:pt-0 lg:h-96' data-aos = "fade-up" src={Me} alt="Vishal" />
+              <img className=' h-auto rounded-lg  pt-2 border-4 border-white md:pt-0 lg:h-96 ' onContextMenu={(e) => e.preventDefault()} data-aos = "fade-up" src={Me} alt="Vishal" />
             </div>
 
             <div className=' space-y-4 md:text-2xl text-white pt-4 poppins-font' data-aos = "fade-up"> 
@@ -53,27 +53,32 @@ const About = () => {
             
           </div>
         </div>
-        {/* <div className=" pt-12 pb-12">  
-          <div className=" lg:flex justify-center items-center p-3  mx-3 md:mx-20 md:mt-12 rounded-lg flex-shrink-0 md:space-x-32 hover:scale-105 hover:border-2 hover:border-cyan-300" data-aos = "flip-right" style={textBG}>
-            <img className=' h-auto rounded-lg pt-2 md:pt-0 md:h-96' data-aos = "fade-up" src={Tools} alt="Tools" />
-            <p className=' md:text-2xl text-white poppins-font' data-aos = "fade-up"> 
-            I possess extensive proficiency in utilizing Tailwind CSS, JavaScript, and prominent frameworks such as React.js and Django, alongside application frameworks like React Native. Furthermore, I am adept in programming languages including Python, C, C++, and Java. My comfort extends to leveraging design tools like Figma, Canvas, Miro, and Mural to craft intuitive user interfaces and facilitate seamless ideation processes.           
-            </p>
-          </div>
-        </div> */}
+    
         <div className="" data-aos = " fade-up">
           <p className=' flex justify-center poppins-font text-2xl md:text-4xl font-semibold text-white p-10' >Stacks & Tools I use</p>
           <Skills />
         </div>
-        <div className=" mr-4 flex-row justify-center mt-32 ">
-            <p className=' text-xl flex justify-center poppins-font md:text-3xl text-white font-medium'>Connect with me on:</p>
-            <div className=" flex justify-center space-x-6 p-6" >
-              <a className=' hover:scale-105 transition-transform' href="https://github.com/vishalmet" target='_blank'><img className=' h-8 md:h-10' src={GitHub}></img></a>
-              <a className=' hover:scale-105 transition-transform' href="https://www.linkedin.com/in/vishal-aakash/"  target='_blank'><img className=' h-8 md:h-10'  src={LinkedIn}></img></a>
-              <a className=' hover:scale-105 transition-transform' href="https://twitter.com/VishalAakash18" target='_blank'><img className=' h-8 md:h-10' src={X}></img></a>
-              <a className=' hover:scale-105 transition-transform' href="https://youtube.com/@vishalaakash1802" target='_blank'><img className=' h-8 md:h-10' src={Yt}></img></a>
+       
+        <div className=" flex-row justify-center mt-32 poppins-font">
+          <p className=' text-xl flex justify-center md:text-3xl text-white font-medium'>Connect with me on:</p>
+          <div className=" text-white flex justify-center space-x-10  mt-6">
+            <div className=" p-3 rounded-md w-44 flex justify-center border border-white hover:border-cyan-400" style={textBG} >
+              <a className=' flex items-center text-lg md:text-2xl hover:scale-105 transition-transform' href="https://github.com/vishalmet" target='_blank'> Github<img className='pl-3 h-8 md:h-10' src={GitHub}></img></a>
             </div>
+            <div className=" p-3 rounded-md w-44 border border-white hover:border-cyan-400" style={textBG} >
+              <a className=' flex  items-center text-lg md:text-2xl hover:scale-105 transition-transform' href="https://www.linkedin.com/in/vishal-aakash/"  target='_blank'>LinkedIn<img className=' pl-3 h-8 md:h-10'  src={LinkedIn}></img></a>
+            </div>
+          </div>
+          <div className=" text-white flex justify-center space-x-10  mt-6">
+            <div className=" p-3 rounded-md w-44 flex justify-center border border-white hover:border-cyan-400" style={textBG} >
+              <a className=' flex  items-center text-lg md:text-2xl hover:scale-105 transition-transform' href="https://twitter.com/VishalAakash18" target='_blank'>X<img className='pl-3 h-8 md:h-10' src={X}></img></a>
+            </div>
+            <div className=" p-3 rounded-md w-44 border border-white hover:border-cyan-400 hover:shadow-glow" style={textBG} >
+              <a className=' flex  items-center text-lg md:text-2xl hover:scale-105 transition-transform hover:shadow-glow' href="https://youtube.com/@vishalaakash1802" target='_blank'>Youtube<img className='pl-3 h-8 md:h-10' src={Yt}></img></a>
+            </div>
+          </div>
         </div>
+        <br />
 
         <div className=" bg-black flex justify-center">
           <p className=' text-white poppins-font p-3'> Designed & Developed by Vishal Aakash</p>
