7c69d9bc20a7c2d7fc8147aacd7672744f39ade7
vishalmet <vishalmet003@gmail.com>
2024-03-14 11:02:30 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,16 +34,23 @@ const Navbar = () => {
                     ☰ 
                 </button>
             </div>
-            <div className={`lg:hidden fixed inset-0 bg-gray-900 bg-opacity-50 z-50 ${isOpen ? 'block' : 'hidden'}`} onClick={closeMenu}></div>
-            <div className={`lg:hidden absolute inset-y-0 left-0 transform transition duration-300 ease-in-out bg-white w-64 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+            {/* <div className={`lg:hidden fixed inset-0 bg-gray-900 bg-opacity-50 z-50 ${isOpen ? 'block' : 'hidden'}`} onClick={closeMenu}></div> */}
+            <div className={`lg:hidden absolute inset-y-0 z-50 left-0 transform transition duration-300 ease-in-out bg-white w-64 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
              <button onClick={handleCloseClick} className="absolute top-0 right-0 m-4 text-2xl focus:outline-none">×</button>
                 <ul className="p-4 space-y-2 poppins-font font-medium">
                     <li><Link to="/" className='py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black' >Home</Link></li>
-                    <li><Link to="#about-section" className='py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black'>About Me</Link></li>
+                    {/* <li><Link to="#about-section" className='py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black'>About Me</Link></li> */}
                     <li><Link to="/experience" className='py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black'>Experience</Link></li>
                     <li><Link to="/works" className='py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black'>My Works</Link></li>
                     <li><Link to="/contact" className='py-1 px-1 mx-1 pt-2 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black'>Contact</Link></li>
-                    <a href="https://drive.google.com/file/d/1SXoG2med5vyVV4orem7vDVyDefa5qItY/view?usp=drive_link" target='_blank' className=' py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 rounded-md cursor-pointer font-semibold hover:from-sky-400 hover:to-sky-200 hover:text-black poppins-font'>Resume</a>
+                    <div className=" ">
+                        <div className=" mt-3">
+                            <a href="https://drive.google.com/file/d/1SXoG2med5vyVV4orem7vDVyDefa5qItY/view?usp=drive_link" target='_blank' className=' py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 rounded-md cursor-pointer font-semibold hover:from-sky-400 hover:to-sky-200 hover:text-black poppins-font'>Resume/CV 📃</a>
+                        </div>
+                        <div className=" mt-3">
+                            <a href="https://drive.google.com/drive/folders/1gSkpVjMNAO3Yw4Odht6OZyKvrOAXJPDe?usp=sharing" target='_blank' className=' py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 rounded-md cursor-pointer font-semibold hover:from-sky-400 hover:to-sky-200'>Certificates 👨🏻‍💻</a>
+                        </div>
+                    </div>
                 </ul>
             </div>
             {/* Desktop Navbar */}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -62,18 +62,18 @@ const About = () => {
         <div className=" flex-row justify-center mt-32 poppins-font">
           <p className=' text-xl flex justify-center md:text-3xl text-white font-medium'>Connect with me on:</p>
           <div className=" text-white flex justify-center space-x-10  mt-6">
-            <div className=" p-3 rounded-md w-44 flex justify-center border border-white border-glow"  >
+            <div className=" p-3 rounded-md w-36 md:w-44 flex justify-center border border-white border-glow"  >
               <a className=' flex items-center text-lg md:text-2xl hover:scale-105 transition-transform' href="https://github.com/vishalmet" target='_blank'> Github<img className='pl-3 h-8 md:h-10' src={GitHub}></img></a>
             </div>
-            <div className=" p-3 rounded-md w-44 border border-white border-glow"  >
+            <div className=" p-3 rounded-md w-36 md:w-44 border border-white border-glow"  >
               <a className=' flex  items-center text-lg md:text-2xl hover:scale-105 transition-transform' href="https://www.linkedin.com/in/vishal-aakash/"  target='_blank'>LinkedIn<img className=' pl-3 h-8 md:h-10'  src={LinkedIn}></img></a>
             </div>
           </div>
           <div className=" text-white flex justify-center space-x-10  mt-6">
-            <div className=" p-3 rounded-md w-44 flex justify-center border border-white border-glow"  >
+            <div className=" p-3 rounded-md w-36 md:w-44 flex justify-center border border-white border-glow"  >
               <a className=' flex  items-center text-lg md:text-2xl hover:scale-105 transition-transform' href="https://twitter.com/VishalAakash18" target='_blank'>X<img className='pl-3 h-8 md:h-10' src={X}></img></a>
             </div>
-            <div className=" p-3 rounded-md w-44 border border-white hover:shadow-glow border-glow"  >
+            <div className=" p-3 rounded-md w-36 md:w-44 border border-white hover:shadow-glow border-glow"  >
               <a className=' flex  items-center text-lg md:text-2xl hover:scale-105 transition-transform hover:shadow-glow' href="https://youtube.com/@vishalaakash1802" target='_blank'>Youtube<img className='pl-3 h-8 md:h-10' src={Yt}></img></a>
             </div>
           </div>
